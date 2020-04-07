60de83fb1b793f3c5878da6001fc6f49b1964bb9
Serhii Matiushenko <matyushenko.sergiy@gmail.com>
2020-04-07 18:06:30 +0300

    dns provider

--- providers/dns/dns.tf
+++ providers/dns/dns.tf
@@ -6,3 +6,21 @@ resource "dns_a_record_set" "lb" {
   ]
   ttl = 300
 }
+
+resource "dns_a_record_set" "static" {
+  zone = "devopsua.tk."
+  name = "static"
+  addresses = [
+    "172.16.160.142",
+  ]
+  ttl = 300
+}
+
+resource "dns_a_record_set" "gitlab" {
+  zone = "devopsua.tk."
+  name = "gitlab"
+  addresses = [
+    "172.16.160.142",
+  ]
+  ttl = 300
+}
